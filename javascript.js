d89927e9e33c6285b676131497789766bc6fdcfa
Jackson Jewell <jackson.t.jewell@gmail.com>
2019-11-21 21:31:33 -0600

    all functions complete

--- javascript.js
+++ javascript.js
@@ -1,12 +1,32 @@
 let formArray = ["nineAm", "tenAm", "elevenAm", "noon", "onePm", "twoPm", "threePm", "fourPm", "fivePm"];
 let timeVar;
+let dateVar;
+
+function dateFinder(){
+    dateVar = new Date().toLocaleDateString();
+    return dateVar;
+}
+
+dateFinder();
+console.log(dateVar);
+
+$("#dateDisplay").text(dateVar);
 
 formArray.forEach(startUp);
 
 function startUp(storageId){
     var formSet = document.getElementById(storageId);
     var textSet = localStorage.getItem(storageId);
+    var currentDate = localStorage.getItem("setDate")
+    if (textSet == null){
+    }else{
     formSet.setAttribute("value",textSet);
+    }
+
+    if(currentDate !== dateVar){
+        localStorage.clear();
+    }else{
+    }
 }
 
 function logText(formReturn){
@@ -15,12 +35,14 @@ function logText(formReturn){
     var inputText= document.getElementById(formId).value;
     var storageClass = document.getElementById(formId).name;
     localStorage.setItem(storageClass, inputText);
+    localStorage.setItem(setDate, dateVar);
 }
 
 function timeFinder(){
-    timeVar = new Date().toLocaleTimeString([],{hour: '2-digit', minute: '2-digit'});
+    timeVar = new Date().toLocaleTimeString([],{hour: '2-digit'});
     return timeVar;
 }
 
 timeFinder();
 console.log(timeVar);
+
