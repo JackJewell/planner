7822aaeedff84652cc0980a9b144da365b581648
Jackson Jewell <jackson.t.jewell@gmail.com>
2019-11-23 12:08:07 -0600

    Hi Jackson

--- javascript.js
+++ javascript.js
@@ -43,6 +43,59 @@ function timeFinder(){
     return timeVar;
 }
 
-timeFinder();
-console.log(timeVar);
 
+
+
+function timeStyle(){
+    switch(timeVar){
+        case "09 AM":
+            $("#nineAm").css({"background-color": "#ffcccb"});
+            changeColors(1);
+            break;
+        case "10 AM":
+            $("#tenAm").css({"background-color": "#ffcccb"});
+            changeColors(2);
+            break;
+        case "11 AM":
+            $("#elevenAm").css({"background-color": "#ffcccb"});
+            changeColors(3);
+            break;
+        case "12 PM":
+            $("#noon").css({"background-color": "#ffcccb"});
+            changeColors(4);
+            break;
+        case "01 PM":
+            $("#onePm").css({"background-color": "#ffcccb"});
+            changeColors(5);
+            break;
+        case "02 PM":
+            $("#twoPm").css({"background-color": "#ffcccb"});
+            changeColors(6);
+            break;
+        case "03 PM":
+            $("#threePm").css({"background-color": "#ffcccb"});
+            changeColors(7);
+            break;
+        case "04 PM":
+            $("#fourPm").css({"background-color": "#ffcccb"});
+            changeColors(8);
+            break;
+        case "05 PM":
+            $("#fivePm").css({"background-color": "#ffcccb"});
+            changeColors(9);
+            break;
+    }
+}
+
+function changeColors(switcher){
+    var counter = 9-switcher;
+    for(var i = 0; i < counter;){
+        var chooser = $("#"+formArray[switcher]);
+        chooser.css({"background-color": "#90ee90"});
+        switcher++;
+        i++;
+    }
+}
+
+timeFinder();
+timeStyle();
